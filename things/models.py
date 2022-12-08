76d3616a31f10a0b4d3ddea72ccd59bded507bd9
heliip812 <heliip812@gmail.com>
2022-12-08 14:01:46 +0000

    a

--- things/models.py
+++ things/models.py
@@ -1,8 +1,7 @@
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 
-class Transfer(models.Model):
-    """Bank Transfer Model"""
+class Thing(models.Model):
     name = models.CharField(max_length=30, unique=True, blank=False)
     description = models.CharField(max_length=120, unique=False, blank=True)
     quantity = models.IntegerField(unique=False, validators=[
