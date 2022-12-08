a1daf54e2fd3df04fb0b32c809d0f0be4637b881
heliip812 <heliip812@gmail.com>
2022-12-08 03:04:56 +0000

    Exercise 2

--- things/models.py
+++ things/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
+class Transfer(models.Model):
+    """Bank Transfer Model"""
+    name = models.CharField(max_length=50)
+    description = models.CharField(max_length=200)
+    name = models.IntegerField(max_length=30)
+    
 # Create your models here.
